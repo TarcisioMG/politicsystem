b750c182655106e6e086f236b949c621a1bd1b32
TarcisioMG <tarcisiomarques00000@gmail.com>
2018-11-13 21:37:57 -0300

    Ajustes

--- resources/views/candidato/confirmar.blade.php
+++ resources/views/candidato/confirmar.blade.php
@@ -12,9 +12,9 @@
     <table class="table table-striped">
     <thead>
       <tr>
+        <th>Foto</th>
         <th>Nome Completo</th>
         <th>Nome Exibição</th>
-        <th>Foto</th>
         <th>Partido ID</th>
         <th>Número</th>
       </tr>
@@ -22,16 +22,16 @@
     <tbody>
       
       <tr>
+        <td><img src="{{$candidato['foto']}}" style="width: 75px; height: 50px;"></td>
         <td>{{$candidato['nome_completo']}}</td>
         <td>{{$candidato['nome_exibicao']}}</td>
-        <td><img src="{{$candidato['foto']}}" style="width: 75px; height: 50px;"></td>
         <td>{{$candidato['id_partido']}}</td>
         <td>{{$candidato['numero_candidato']}}</td>
       </tr>
     </tbody>
   </table>
-    <a href="{{action('CandidatoController@confirmar', $candidato['id'])}}" class="btn btn-success">Confirmar</a>
     <a href="{{action('CandidatoController@view_votar')}}" class="btn btn-danger">Corrigir</a>
+    <a href="{{action('CandidatoController@confirmar', $candidato['id'])}}" class="btn btn-success btn-lg">Confirmar</a>
   </form>
 </div>
 @endsection
