edbb00d7d9bab9b628d2f4afdcde8fe94bab197d
TarcisioMG <tarcisiomarques00000@gmail.com>
2018-11-15 19:26:55 -0300

    Atualização layout

--- resources/views/candidato/confirmar.blade.php
+++ resources/views/candidato/confirmar.blade.php
@@ -2,40 +2,32 @@
 
 @section('content')
     <div class="container">
-        <img src="{{$candidato['foto']}}" class="rounded float-right" style="width: 300px; height: 200px;">
-    </div>
-
-
-
-    <br />
-    
     @if (\Session::has('success'))
       <div class="alert alert-success">
         <p>{{ \Session::get('success') }}</p>
       </div><br />
      @endif
-    <table class="table table-striped">
-    <thead>
-      <tr>
-        <th>Foto</th>
-        <th>Nome Completo</th>
-        <th>Nome Exibição</th>
-        <th>Partido ID</th>
-        <th>Número</th>
-      </tr>
-    </thead>
-    <tbody>
-      
-      <tr>
-        <td>{{$candidato['nome_completo']}}</td>
-        <td>{{$candidato['nome_exibicao']}}</td>
-        <td>{{$candidato['id_partido']}}</td>
-        <td>{{$candidato['numero_candidato']}}</td>
-      </tr>
-    </tbody>
-  </table>
-    <a href="{{action('CandidatoController@view_votar')}}" class="btn btn-danger">Corrigir</a>
-    <a href="{{action('CandidatoController@confirmar', $candidato['id'])}}" class="btn btn-success">Confirmar</a>
+        <h1 style="text-align: center">Confirmar Voto</h1>
+        <br><br>
+        <div class="col-md-12s">
+          <img src="{{$candidato['foto']}}" class="rounded float-right" style="width: 300px; height: 200px;">
+        </div>
+        <div class="col-md-12" style="font-size: 30px">
+          Número: {{$candidato['numero_candidato']}} <br>
+          Nome Completo: {{$candidato['nome_completo']}} <br>
+          Nome de Exibição: {{$candidato['nome_exibicao']}} <br>
+          ID Partido: {{$candidato['id_partido']}} <br>
+        </div>
+        <br><br><br><br><br>
+        <div class="row justify-content-center">
+          <div class="col-4">
+            <a href="{{action('CandidatoController@view_votar')}}" class="btn btn-danger btn-lg">Corrigir</a>
+          </div>
+          <div class="col-0">
+            <a href="{{action('CandidatoController@confirmar', $candidato['id'])}}" class="btn btn-success btn-lg">Confirmar</a>
+          </div>
+        </div>
+    </div>
   </form>
 </div>
 @endsection
